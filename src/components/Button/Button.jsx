984c9a92e505ea0e3ff2bafc6482783f68a94eaa
kretova0319 <kretova0319@gmail.com>
2024-11-18 14:10:35 +0400

    fix: disabled (button Save)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./button.module.css";
 
-export default function Button({ text, color, handler, id }) {
+export default function Button({ text, color, handler, id, disabled }) {
   return (
     <button
       className={`${styles.btnTable} ${styles[color]}`}
       onClick={() => handler(id)}
+      disabled={disabled}
     >
       {text}
     </button>
